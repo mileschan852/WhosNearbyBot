ad02be3e464e31082f8b25839aa43412ef291012
Miles Chan <118529335+mileschan852@users.noreply.github.com>
2026-07-31 09:18:48 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// I am forcing relative paths here so GitHub Pages can actually find your files.
 export default defineConfig({
   plugins: [react()],
-  base: '/whos-nearby/',
+  base: './', 
 })
