35573a1cdade9935bc4939f94b43993b458d9c73
Miles Chan <mileschan852@users.noreply.github.com>
2026-08-22 19:41:25 +0000

    Fix unused imports (React, useEffect, useState) flagged by tsc

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import { useState, useCallback } from 'react'
 import styled, { createGlobalStyle } from 'styled-components'
 import { TonConnectUIProvider, useTonConnectUI, useTonAddress } from '@tonconnect/ui-react'
 
